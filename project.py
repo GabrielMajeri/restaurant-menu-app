f65f5106dbec552eb8c8c07b46ec8bc0129429b5
Gabriel Majeri <gabriel.majeri6@gmail.com>
2021-01-17 14:43:00 +0200

    Set up Flask routing

--- project.py
+++ project.py
@@ -8,20 +8,31 @@ app = Flask(__name__)
 session = DBSession()
 
 
-@app.route('/')
-@app.route('/hello')
-def hello_world():
+@app.route('/restaurant/<int:restaurant_id>/')
+def view_menu_items(restaurant_id):
     output = ''
-    restaurants = session.query(Restaurant)
-    for restaurant in restaurants:
-        output += f'<h2>{restaurant.name}</h2>'
-        items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
-        for i in items:
-            output += f'{i.name} <br/>'
-            output += f'{i.price or ""} <br/>'
-            output += f'{i.description or ""} <br/>'
-        output += '<br/>'
+    items = session.query(MenuItem).filter_by(restaurant_id=restaurant_id)
+    for i in items:
+        output += f'{i.name} <br/>'
+        output += f'{i.price or ""} <br/>'
+        output += f'{i.description or ""} <br/>'
+    output += '<br/>'
     return output
+
+
+@app.route('/restaurant/<int:restaurant_id>/new/')
+def add_menu_item(restaurant_id):
+    return 'Add a new menu item'
+
+
+@app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/edit/')
+def edit_menu_item(restaurant_id, menu_id):
+    return 'Edit a menu item'
+
+
+@app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/delete/')
+def delete_menu_item(restaurant_id, menu_id):
+    return 'Delete a menu item'
 
 
 if __name__ == '__main__':
